f1c282e4e2b8017ad2c9addececcda9fa9d23848
Sxnny-s <ethsok@protonmail.com>
2026-01-24 19:29:28 -0500

    Medium leetcode tree problem

---
+++ 515.cpp
@@ -0,0 +1,28 @@
+// Find the largert element in every level of a binary tree
+#include <queue>
+class Solution{
+    public:
+       vector<int> largestValues(TreeNode* root){
+
+        vector<int> res{};
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(!q.empty()){
+            int largest {INT_MIN};
+            size_t level_size {q.size()};
+            for(size_t i {}; i < level_size;i++){
+                TreeNode* node {q.front()};
+                q.pop();
+                largest = max(largest,node->val);
+
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
+            res.push_back(largest);
+        }
+        return res;
+
+       }
+}
