492940875dc6453f08708978cde911a4c51a6d80
Sxnny-s <ethsok@protonmail.com>
2026-01-27 09:26:41 -0500

    Added untracked files

--- 1200.cpp
+++ 1200.cpp
@@ -13,7 +13,7 @@ public:
 
         for(int i {}; i < arr.size() - 1;i++){
 
-            vector<int> pair {arr[i], arr[i + 1]};
+            vector<int> pair {arr[i], arr[i + 1 ]};
             if(arr[i+ 1] - arr[i] == min_diff){
                 res.push_back(pair);
 
